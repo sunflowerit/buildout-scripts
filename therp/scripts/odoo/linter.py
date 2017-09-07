221585879379402fa396af4bbc939aea04c2953f
Tom Blauwendraat <antiflu@gmail.com>
2017-09-07 22:36:46 +0200

    fix

--- therp/scripts/odoo/linter.py
+++ therp/scripts/odoo/linter.py
@@ -7,7 +7,6 @@ from pylint.lint import Run
 def do_pylint():
     parser = ArgumentParser()
     parser.add_argument('path_to_lint', metavar='folder', type=str,
-                        required=True,
                         help='the folder of the module(s) to run PyLint on')
     arguments = parser.parse_args()
     cur_path = os.getcwd()
