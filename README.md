bf4c5d5781495ffe3c7dae4e37dc30b23a8d1b12
Tom Blauwendraat <antiflu@gmail.com>
2017-09-07 17:57:35 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Buildout Scripts Odoo
+
+These scripts can be developed and used inside of an Odoo buildout recipe.
+
+### Regenerating the package files
+
+After modifying something in this package, run:
+
+```
+python setup.py sdist
+```
---
+++ therp/scripts/odoo/pylint.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from argparse import ArgumentParser
+import os 
+from pylint.lint import Run
+
+
+def do_pylint(session):
+    parser = ArgumentParser()
+    parser.add_argument('path_to_lint', metavar='folder', type=str,
+                        required=True,
+                        help='the folder of the module(s) to run PyLint on')
+    arguments = parser.parse_args()
+    cur_path = os.getcwd()
+    path_to_lint = os.path.join(cur_path, arguments.path_to_lint)
+    Run(['--rcfile=.pylint.cfg', path_to_lint])
+
