ca9201073996e4ff26ba6bd5fbea3c830a7d34e7
Tom Blauwendraat <antiflu@gmail.com>
2017-09-08 01:22:10 +0200

    fix

--- therp/scripts/odoo/linter.py
+++ therp/scripts/odoo/linter.py
@@ -6,10 +6,12 @@ from pylint.lint import Run
 
 def do_pylint():
     parser = ArgumentParser()
-    parser.add_argument('path_to_lint', metavar='folder', type=str,
-                        help='the folder of the module(s) to run PyLint on')
+    parser.add_argument('repo_to_lint', metavar='repo', type=str,
+                        help='the name of the repo to run PyLint on')
     arguments = parser.parse_args()
     cur_path = os.getcwd()
-    path_to_lint = os.path.join(cur_path, arguments.path_to_lint)
-    Run(['--rcfile=.pylint.cfg', path_to_lint])
+    repo_to_lint = os.path.join(cur_path, 'parts', arguments.path_to_lint)
+    for path_to_lint in os.listdir(repo_to_lint):
+        if not os.path.isfile(path_to_lint):
+            Run(['--rcfile=.pylint.cfg', path_to_lint])
 
